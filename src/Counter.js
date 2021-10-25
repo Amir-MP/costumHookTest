ce1908c47ea526a9b4534a41d088a9002317af17
XVCyberpunKVX <amir.re6@gmail.com>
2021-10-25 20:29:36 +0330

    kkkk

---
+++ src/Counter.js
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+const Counter = () => {
+    const [counterVal,setCounterVal] = useState(0);
+  const increment=()=>{
+    setCounterVal(counterVal + 1);
+  }
+  return {counterVal,increment,setCounterVal}
+}
+
+export default Counter
